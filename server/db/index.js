75c1b33b6b982a8c56b0644e5d5c52e762e8bac7
Aidan Bane <aidanfbane@gmail.com>
2017-03-31 16:14:05 -0700

    (feat) Saving and finding data from MongoDB

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/grouper-main');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('MongoDB -Grouper DB- is now connected to the server...');
+});
+
+module.exports = db;
--- server/routers/apirouter.js
+++ server/routers/apirouter.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
+const path = require('path');
+const Group = require(path.join(__dirname, './../db/models/groupModel.js'));
 
-router.get('/', (req, res) => {
-  res.send('client sent a GET request');
-})
+router.get('/groups', function(req, res) {
+  Group.find().then(function(data) {
+    console.log('GET', data);
+    res.status(200).json(data);
+  }).catch((err) => {
+    console.error('Error fetching group');
+    res.status(501).send(err);
+  });
+});
 
-router.post('/', (req, res) => {
-  res.json(req.body);
-})
+router.post('/groups', (req, res) => {
+  let group = {
+      groupName: req.body.groupName,
+      location: req.body.location,
+      eventType: req.body.eventType
+    };
+
+  new Group(group).save().then(function(data) {
+    console.log('POST', data);
+    res.status(201).json(data);
+  }).catch((err) => {
+    console.error('Error POSTing new client group');
+    res.status(501).send(err);
+  });
+});
 
 module.exports = router;
