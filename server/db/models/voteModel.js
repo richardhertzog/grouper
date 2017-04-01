ea16fc8dcb8b74e55afde2656df41c7018f6e2b9
Aidan Bane <aidanfbane@gmail.com>
2017-04-01 14:34:48 -0700

    (feat) Added votesModeal.js

---
+++ server/db/models/voteModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const path = require('path')
+const db = require(path.join(__dirname, '../index.js'))
+
+const voteSchema = mongoose.Schema({
+  groupId: String,
+  votes: [{
+    id: {
+      type: String,
+      required: true
+    },
+    vote: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }]
+})
+
+let Vote = mongoose.model('Group', voteSchema)
+
+module.exports = Vote
