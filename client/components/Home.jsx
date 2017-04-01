9df5f5cc1244686eb3ab9d801994241d3291ff74
Richard Hertzog <richard.hertzog@gmail.com>
2017-04-01 09:48:19 -0700

    home page transition

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Link to="/makeGroup">Make Group Will Become Button</Link>
-        <br></br>
-        <Link to="/voting">Votes Page Test Link</Link>
+        <ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          <Link to="/makeGroup">Make Group Will Become Button</Link>
+          <br></br>
+          <Link to="/voting">Votes Page Test Link</Link>
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
