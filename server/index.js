a0c6b512c6bcaafb4350e8de2dfe288dd524793a
anirvan90 <anirvan.awatramani@gmail.com>
2017-03-31 18:08:15 -0700

    (feat) yelp api request, config.js env variables

--- server/index.js
+++ server/index.js
@@ -19,5 +19,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('server listening on port:', PORT);
 });
-
-
--- server/routers/apirouter.js
+++ server/routers/apirouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const path = require('path');
 const Group = require(path.join(__dirname, './../db/models/groupModel.js'));
+require(path.join(__dirname, './../config.js'))();
+const yelp = require('yelp-fusion');
 
 router.get('/groups', function(req, res) {
   Group.find().then(function(data) {
@@ -13,19 +15,41 @@ router.get('/groups', function(req, res) {
 });
 
 router.post('/groups', (req, res) => {
-  let group = {
-      groupName: req.body.groupName,
-      location: req.body.location,
-      eventType: req.body.eventType
-    };
+  
+  const clientId = process.env.YELP_APP_ID;
+  const clientSecret = process.env.YELP_APP_SECRET;
+  
+  const searchRequest = {
+    term: req.body.eventType,
+    location: req.body.location
+  };
 
-  new Group(group).save().then(function(data) {
-    console.log('POST', data);
-    res.status(201).json(data);
-  }).catch((err) => {
-    console.error('Error POSTing new client group');
-    res.status(501).send(err);
+  yelp.accessToken(clientId, clientSecret).then(response => {
+    const client = yelp.client(response.jsonBody.access_token);
+
+    client.search(searchRequest).then(response => {
+      const firstResult = response.jsonBody.businesses[0];
+      const prettyJson = JSON.stringify(firstResult, null, 4);
+      console.log(prettyJson);
+      res.json(response);
+    });
+  }).catch(e => {
+    console.log(e);
   });
+  // let group = {
+  //     groupName: req.body.groupName,
+  //     location: req.body.location,
+  //     eventType: req.body.eventType,
+  //     yelpApiContent: data
+  //   };
+
+  // new Group(group).save().then(function(data) {
+  //   console.log('POST', data);
+  //   res.status(201).json(data);
+  // }).catch((err) => {
+  //   console.error('Error POSTing new client group');
+  //   res.status(501).send(err);
+  // });
 });
 
 module.exports = router;
