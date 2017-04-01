db0b8e130943504f04b0e363e158db4b011581f8
Aidan Bane <aidanfbane@gmail.com>
2017-04-01 14:06:12 -0700

    (feat) test init

--- server/spec.js
+++ server/spec.js
@@ -5,13 +5,25 @@ const chai = require('chai').expect
 describe('[apiRouter]', function () {
   it('should GET all groups', function (done) {
     request(app)
-      .get('/api/groups')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, resp) {
-        chai(resp.body).to.be.an('array')
-        done()
-      })
+    .get('/api/groups')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, resp) {
+      chai(resp.body).to.be.an('array')
+      done()
+    })
+  })
+
+  it('should GET one group', function (done) {
+    request(app)
+    .get('/api/groups/testyasdf')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, resp) {
+      chai(resp.body).to.be.an('array')
+      done()
+    })
   })
 })
