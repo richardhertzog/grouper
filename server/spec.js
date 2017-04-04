729961d143125fbc8354c73569553987e7c2176d
Aidan Bane <aidanfbane@gmail.com>
2017-04-03 19:01:56 -0700

    (feat) added POST test and test mongodb

--- server/spec.js
+++ server/spec.js
@@ -1,9 +1,29 @@
 process.env.NODE_ENV = 'testing'
+
 const app = require('./index.js')
 const request = require('supertest')
 const chai = require('chai').expect
 
 describe('[apiRouter]', function () {
+  const testGroup = {
+    'groupName': 'test',
+    'location': '611 Mission St. San Francisco, CA',
+    'eventType': 'bar'
+  }
+
+  it('should create a new group on a POST to /api/groups', function (done) {
+    request(app)
+    .post('/api/groups')
+    .send(testGroup)
+    .expect(201)
+    .expect('Content-Type', /json/)
+    .end(function (err, resp) {
+      if (err) done(err)
+      chai(resp.body).to.be.an('object')
+      done()
+    })
+  })
+
   it('should GET all groups', function (done) {
     request(app)
     .get('/api/groups')
@@ -11,19 +31,21 @@ describe('[apiRouter]', function () {
     .expect('Content-Type', /json/)
     .expect(200)
     .end(function (err, resp) {
+      if (err) throw err
       chai(resp.body).to.be.an('array')
       done()
     })
   })
 
-  xit('should GET one group', function (done) {
+  it('should GET one group', function (done) {
     request(app)
-    .get('/api/groups/testyasdf')
+    .get('/api/groups/test')
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
     .expect(200)
     .end(function (err, resp) {
-      chai(resp.body).to.be.an('array')
+      if (err) throw err
+      chai(resp.body).to.be.an('object')
       done()
     })
   })
