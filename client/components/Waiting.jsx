9cb2f56b19064e4c135b2e05cdb23cc1d78cfe85
Aidan Bane <aidanfbane@gmail.com>
2017-04-06 11:33:30 -0700

    (doc) added Waiting.jsx

---
+++ client/components/Waiting.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+class Waiting extends Component {
+  render () {
+    return (
+      <div>
+        <p>Waiting.jsx Waiting for other users etc ......</p>
+      </div>
+    )
+  }
+}
+
+Waiting.propTypes = {
+
+}
+
+export default Waiting
