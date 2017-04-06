247e2cb19968f778ce0884f4de3012abdd28e965
Aidan Bane <aidanfbane@gmail.com>
2017-04-06 14:17:03 -0700

    (feat) Saved YelpData to SessionStore

--- client/components/RootVoting.jsx
+++ client/components/RootVoting.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import Voting from './Voting.jsx'
+import axios from 'axios'
 
 class componentName extends Component {
   constructor (props) {
@@ -9,14 +10,26 @@ class componentName extends Component {
       groupName: ''
     }
   }
-  componentDidMount () {
-    this.setState({groupName: this.props.location.pathname })
+
+  componentWillMount () {
+    axios.get('/api/groups/' + this.props.location.pathname.slice(8))
+    .then((res) => {
+      console.log(res.data)
+      sessionStorage.setItem('yelpData', JSON.stringify(res.data))
+    })
+    .catch((err) => {
+      console.error('axios get error', err)
+    })
+
+    this.setState({ groupName: this.props.location.pathname })
   }
+
   render () {
     return (
       <div>
         <h1>RootVoting</h1>
         <p>{this.state.groupName}</p>
+        <p>{}</p>
         <Router>
           <div>
             <Route path='/waiting' render={() => { return <Waiting name='ZergRUSH!!' /> }} />
