8907147df88e9d1ea54eef3566e032513110b11a
Richard Hertzog <richard.hertzog@gmail.com>
2017-04-12 16:51:46 -0700

    pass auth props from root to nav component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom'
 import Nav from './Nav.jsx'
 
 class Home extends Component {
+  constructor (props) {
+    super(props)
+  }
+
   render () {
     return (
       <div className='card'>
         <div className='card-block mx-auto'>
           <div className='form-group row mx-auto'>
             <div className='btn-group btn-group-md'>
-              <Nav />
+              <Nav auth={this.props.auth} />
             </div>
             <div className='btn-group btn-group-md mr-2'>
               <Link to='/makeGroup' className='btn btn-primary'>New Group</Link>
