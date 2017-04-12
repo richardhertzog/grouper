b881429b0e79df58971a3825d8297d3f2718aad9
Erik Thompson <erik.james.thompson@gmail.com>
2017-04-12 16:46:49 -0700

    Dynamic minute selector with full functionality

--- client/components/MakeGroup.jsx
+++ client/components/MakeGroup.jsx
@@ -9,20 +9,25 @@ class MakeGroup extends Component {
       groupName: '',
       businessType: '',
       location: '',
+      endTime: 2,
       renderVote: false
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.businessClick = this.businessClick.bind(this)
+    this.changeTime = this.changeTime.bind(this)
   }
 
   handleSubmit (event) {
     event.preventDefault()
+    let time = this.state.endTime
+    time = time * 60 * 1000 + Date.now()
     axios.post('/api/groups',
       { groupName: this.state.groupName,
         location: this.state.location,
-        eventType: this.state.businessType
+        eventType: this.state.businessType,
+        endTime: time
       })
     .then(() => {
       this.setState({ renderVote: true })
@@ -39,6 +44,12 @@ class MakeGroup extends Component {
   businessClick (event) {
     event.preventDefault()
     this.setState({ businessType: event.target.id })
+  }
+
+  changeTime (event) {
+    event.preventDefault()
+    let time = Number(event.target.value) + this.state.endTime
+    this.setState({endTime: time})
   }
 
   render () {
@@ -68,6 +79,11 @@ class MakeGroup extends Component {
                 <button className='btn btn-primary' id='parks' onClick={this.businessClick}>Parks</button>
               </div>
             </div>
+              <div className='btn-group btn-group-md'>
+                <button className='btn btn-primary' value={1} id='plus' onClick={this.changeTime}>+</button>
+                <h3>{this.state.endTime}</h3>
+                <button className='btn btn-primary' value={-1} id='minus' onClick={this.changeTime}>-</button>
+              </div>
             <div className='form-group row mx-auto'>
               <div className='btn-block btn-md'>
                 <button className='btn btn-primary btn-md btn-block' id='submit' onClick={this.handleSubmit}>Submit</button>
