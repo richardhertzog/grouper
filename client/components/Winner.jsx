740129ec0b88ca0a9a8a37755f02f98081300d2f
anirvan90 <anirvan.awatramani@gmail.com>
2017-04-13 12:26:56 -0700

    button fucntionality works -- needs css

--- client/components/Winner.jsx
+++ client/components/Winner.jsx
@@ -7,9 +7,11 @@ class Winner extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      winBusiness: {}
+      winBusiness: {},
+      isMapShowing: false
     }
     this.populateState = this.populateState.bind(this)
+    this.toggleMap = this.toggleMap.bind(this)
   }
 
   componentWillMount () {
@@ -27,32 +29,49 @@ class Winner extends Component {
     })
   }
 
+  toggleMap (event) {
+    // event.preventDefault()
+    this.setState({isMapShowing: !this.state.isMapShowing})
+  }
+
   render () {
-    return (
-      <div className='card' style={{'width': '400px'}}>
-        {this.props.name}
+    if (this.state.isMapShowing === false) {
+      return (
+        <div className='card' style={{'width': '400px'}} onClick={this.toggleMap}>
+          {this.props.name}
           WINNER
           <img className='card-img-top img-thumbnail' src={this.state.winBusiness.image_url} alt='Business Image' />
-        <div className='card-block'>
-          <div>
+          <div className='card-block'>
+            <div>
 
-            {this.state.winBusiness.categories ? this.state.winBusiness.categories.map((catogs) => {
-              return <div key={catogs.title}>{catogs.title}</div>
-            }) : null }
-            {this.state.winBusiness.location ? this.state.winBusiness.location.display_address.map((add) => {
-              return <div key={add}>{add}</div>
-            }) : null}
+              {this.state.winBusiness.categories ? this.state.winBusiness.categories.map((catogs) => {
+                return <div key={catogs.title}>{catogs.title}</div>
+              }) : null }
+              {this.state.winBusiness.location ? this.state.winBusiness.location.display_address.map((add) => {
+                return <div key={add}>{add}</div>
+              }) : null}
 
+            </div>
+            <p className='card-text'>{this.state.winBusiness.display_phone ? this.state.winBusiness.display_phone : 'Number Not Available'}</p>
+            <h4 className='card-title'>{this.state.winBusiness.name}</h4>
+            <p className='card-text'>{this.state.winBusiness.price}</p>
           </div>
-          <p className='card-text'>{this.state.winBusiness.display_phone ? this.state.winBusiness.display_phone : 'Number Not Available'}</p>
-          <h4 className='card-title'>{this.state.winBusiness.name}</h4>
-          <p className='card-text'>{this.state.winBusiness.price}</p>
         </div>
-        { this.state.winBusiness.coordinates ? <Map lat={this.state.winBusiness.coordinates.latitude} long={this.state.winBusiness.coordinates.longitude} />
-          : null
-        }
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div className='card' onClick={this.toggleMap}>
+          <div className='card-block'>
+            { this.state.winBusiness.coordinates ? <Map lat={this.state.winBusiness.coordinates.latitude} long={this.state.winBusiness.coordinates.longitude} />
+              : null
+            }
+          </div>
+          <div className='btn btn-primary mr-1 rounded-circle btn-circle'>
+            <p>Hello</p>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
