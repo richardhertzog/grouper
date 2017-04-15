0b64087e32eb90133a9573a706d3f1f5ec736d95
Richard Hertzog <richard.hertzog@gmail.com>
2017-04-14 20:52:02 -0700

    refactor signup to use grommet components

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,29 +1,21 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
+import LoginForm from 'grommet/components/LoginForm'
 
 class SignUp extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      username: '',
-      password: '',
       signedIn: false
     }
-    this.handleChange = this.handleChange.bind(this)
+
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange (event) {
-    let { name, value } = event.target
-    let newState = {}
-    newState[name] = value
-    this.setState(newState)
-  }
-
   handleSubmit (event) {
-    event.preventDefault()
-    axios.post('/auth/signup', this.state)
+    delete event.rememberMe
+    axios.post('/auth/signup', event)
     .then((res) => {
       localStorage.setItem('token', res.data.token)
       localStorage.setItem('username', res.data.username)
@@ -40,20 +32,7 @@ class SignUp extends Component {
     }
 
     return (
-      <div>
-        <p>Sign Up</p>
-        <form onSubmit={this.handleSubmit}>
-          <fieldset>
-            <label>Username: </label>
-            <input name='username' type='text' value={this.state.username} onChange={this.handleChange} required />
-          </fieldset>
-          <fieldset>
-            <label>Password: </label>
-            <input name='password' type='password' value={this.state.password} onChange={this.handleChange} required />
-          </fieldset>
-          <button action='submit'>Sign Up!</button>
-        </form>
-      </div>
+      <LoginForm onSubmit={this.handleSubmit} />
     )
   }
 }
