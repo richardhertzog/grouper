ab8d6bf4ef7e54e1a8b2ba7891d1735127f930e2
Aidan Bane <aidanfbane@gmail.com>
2017-04-19 15:29:01 -0700

    (feat) Added back button to About Us

--- client/components/About/About.jsx
+++ client/components/About/About.jsx
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
+
+// grommet components
 import Box from 'grommet/components/Box'
+import Anchor from 'grommet/components/Anchor'
+import Heading from 'grommet/components/Heading'
+
+// icons
+import PreviousIcon from 'grommet/components/icons/base/Previous'
 
 class About extends Component {
   constructor(props) {
     super(props)
   }
-  
+
   render () {
     return (
-      <Box justify='start'
-        align='center'
-        wrap={true}
+      <Box justify='center'  
         pad='medium'
-        margin='small'
-        colorIndex='light-2'>
-        ABOUT.jsx
+        margin='medium'>
+        <Anchor 
+          align='start'
+          path='/'
+          icon={<PreviousIcon />}
+          label='Back' />
+          <Box 
+            align='center'
+            wrap={true}
+            pad='none'
+            margin='none'
+          >
+            <Heading align='center'> About Us </Heading>
+        </Box>
       </Box>
     )
   }
